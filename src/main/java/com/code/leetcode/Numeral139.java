f8a501e79cf3c6658dae179026b07c7c94b894ad
damai <damai@qunhemail.com>
2019-12-11 21:01:55 +0800

    '139'

--- src/main/java/com/code/leetcode/Numeral139.java
+++ src/main/java/com/code/leetcode/Numeral139.java
@@ -8,7 +8,9 @@ package com.code.leetcode;
 
 import com.google.common.collect.Lists;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * @author damai
@@ -19,7 +21,7 @@ public class Numeral139 {
     public static void main(String[] args){
         List<String> list = Lists.newArrayList("apple","pen");
         Numeral139 numeral198 = new Numeral139();
-        System.out.println(numeral198.wordBreak("applepenapple",  list));
+        System.out.println(numeral198.wordBreak2("applepenapple",  list));
     }
 
     private boolean contains  = false;
@@ -42,5 +44,19 @@ public class Numeral139 {
                 }
             }
         }
+    }
+    public boolean wordBreak2(String s, List <String> wordDict) {
+        return word_Break2(s,new HashSet <>(wordDict),0);
+    }
+    public boolean word_Break2(String s, Set <String> wordDict, int start) {
+        if (start == s.length()){
+            return true;
+        }
+        for (int end = start + 1; end <= s.length(); end++){
+            if (wordDict.contains(s.substring(start, end)) && word_Break2(s, wordDict, end)){
+                return true;
+            }
+        }
+        return false;
     }
 }
