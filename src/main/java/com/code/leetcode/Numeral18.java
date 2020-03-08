58869145df4dcc79c712d858cea3ce1aa518d359
senhu <845789673@qq.com>
2020-03-08 16:12:58 +0800

    leetcode 18 pass

--- src/main/java/com/code/leetcode/Numeral18.java
+++ src/main/java/com/code/leetcode/Numeral18.java
@@ -6,6 +6,10 @@
 
 package com.code.leetcode;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -22,9 +26,41 @@ public class Numeral18 {
      * 链接：https://leetcode-cn.com/problems/4sum
      * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
      */
-
+    public List<List<Integer>> result = new ArrayList<>();
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        return null;
+        Arrays.sort(nums);
+        fourSumFullSort(nums, 0, target, new ArrayList<>(), 0);
+        return result;
 
+    }
+    public void fourSumFullSort(int[] nums, int index, int target, List<Integer> integerList, int sum){
+        if (integerList.size() == 4 && sum == target && !result.contains(integerList)){
+            result.add(integerList);
+            return;
+        }
+        if (integerList.size() < 4){
+            for (int i=index; i<nums.length; i++){
+                    List<Integer> list = new ArrayList<>();
+                    list.addAll(integerList);
+                    list.add(nums[i]);
+                    fourSumFullSort(nums, i + 1, target, list, sum + nums[i]);
+            }
+        }
+
+
+    }
+    @Test
+    public void test01(){
+        int[] nums = {1, 0, -1, 0, -2, 2};
+        List<List<Integer>> list = fourSum(nums, 0);
+        System.out.println(String.valueOf(list));
+        Assert.assertTrue(true);
+    }
+    @Test
+    public void test02(){
+        int[] nums = {-5,5,4,-3,0,0,4,-2};
+        List<List<Integer>> list = fourSum(nums, 4);
+        System.out.println(String.valueOf(list));
+        Assert.assertTrue(true);
     }
 }
