5a48fcdfbfa7bc8b7174947df0be5c030cf7fb26
damai <damai@qunhemail.com>
2020-03-26 14:44:22 +0800

    函数接口demo

--- src/main/java/com/code/newfeatures/BooleanFunctionalInterfaceTest.java
+++ src/main/java/com/code/newfeatures/BooleanFunctionalInterfaceTest.java
@@ -9,6 +9,7 @@ package com.code.newfeatures;
 import com.google.common.collect.Lists;
 import org.apache.commons.lang3.StringUtils;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.util.List;
@@ -28,9 +29,16 @@ public class BooleanFunctionalInterfaceTest {
     }
 
     @Test
+    @Ignore
     public void fiterTest(){
         List<String> result = Fiter.filter(list, str -> StringUtils.equals((String)str,
                 "xiaoming"));
         result.stream().forEach(System.out::println);
+    }
+    @Test
+    public void test2(){
+        Object target1 = (Runnable)() -> System.out.println("新建了一个线程");
+        new Thread((Runnable) target1).start();
+        new Thread(()-> System.out.println("新增线程2")).start();;
     }
 }
